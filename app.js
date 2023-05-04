cb2e6e1cae13c7a6261d5883ea2dad56a4d32531
RaulShahi1 <shahi.raul@webexpertsnepal.com>
2023-05-04 12:46:40 +0545

    created schemas

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ const express = require("express");
 const dotenv = require("dotenv");
 const chatRoutes = require("./routes/chats");
 
+const HttpError = require("./models/http-error");
+
 const app = express();
 dotenv.config();
-app.use("/", chatRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Authorization, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST PATCH DELETE");
+  next();
+});
+app.use("/api/chats", chatRoutes);
+
 app.listen(PORT, (err, succ) => {
   console.log(`app running on port ${PORT}`);
+});
+
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route", 404);
+});
+
+app.use((error, req, res, next) => {
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "An unknown error occurred!" });
 });
